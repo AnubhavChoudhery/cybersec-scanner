492b26e0a24f2c9b4714d5eadc2b462ec9e3efe0
Anubhav Choudhery <anubhavchoudhery@gmail.com>
2025-11-26 21:55:47 -0600

    Release v0.1.9 - All dependencies included by default

--- cybersec_scanner/__version__.py
+++ cybersec_scanner/__version__.py
@@ -1,6 +1,6 @@
 """Version information for cybersec-scanner."""
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
 
 __title__ = "cybersec-scanner"
